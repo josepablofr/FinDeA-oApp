cc753f921eca46ac9b028dbeef2a586cfd353092
Jose Pablo Flores <josepablofr@gmail.com>
2023-12-31 17:37:56 -0600

    Fin de año app, repaso retrofit & consumo de API

---
+++ app/src/main/java/com/example/findeaoapp/MainActivity.kt
@@ -0,0 +1,57 @@
+package com.example.findeaoapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.lifecycleScope
+import com.example.findeaoapp.data.RetrofitServiceFactory
+import com.example.findeaoapp.ui.theme.FindeAñoAppTheme
+import kotlinx.coroutines.launch
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val service = RetrofitServiceFactory.makeRetrofitService()
+
+        lifecycleScope.launch {
+            val movies = service.listPopularMovies("9a2f9c1f75a525a717e5a76b81c53d32", "US")
+            println(movies)
+        }
+
+        setContent {
+            FindeAñoAppTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    FindeAñoAppTheme {
+        Greeting("Android")
+    }
+}
---
+++ app/src/main/java/com/example/findeaoapp/data/RetrofitService.kt
@@ -0,0 +1,25 @@
+package com.example.findeaoapp.data
+
+import com.example.findeaoapp.data.model.RemoteResult
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface RetrofitService {
+
+    @GET ("discover/movie?sort_by=popularity.desc")
+    suspend fun listPopularMovies(
+        @Query("api_key") apikey: String,
+        @Query("region") region: String
+    ): RemoteResult
+}
+
+object RetrofitServiceFactory {
+    fun makeRetrofitService(): RetrofitService {
+        return Retrofit.Builder()
+            .baseUrl("https://api.themoviedb.org/3/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build().create(RetrofitService::class.java)
+    }
+}
