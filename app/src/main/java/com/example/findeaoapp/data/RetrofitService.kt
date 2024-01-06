1fb1d5a916ddf1db4dcc405aeee28c7d86380283
Jose Pablo Flores <josepablofr@gmail.com>
2024-01-05 20:22:50 -0600

    Minor improvements

--- app/src/main/java/com/example/findeaoapp/data/RetrofitService.kt
+++ app/src/main/java/com/example/findeaoapp/data/RetrofitService.kt
@@ -10,7 +10,7 @@ interface RetrofitService {
 
     @GET("discover/movie?sort_by=popularity.desc")
     suspend fun listPopularMovies(
-        @Query("api_key") apikey: String,
+        @Query("api_key") apiKey: String,
         @Query("region") region: String
     ): RemoteResult
 
